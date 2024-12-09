af97f5e994c780b119731faa1bf6d2ed175c4861
AnIscoder01 <anisrojo519@gmail.com>
2024-12-09 18:48:03 +0100

    9/12 commit

--- src/app/Services/client.service.ts
+++ src/app/Services/client.service.ts
@@ -1,41 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Client } from '../Interfaces/client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  
+  private API_SERVER = "http://localhost:8081/client";
 
-  private API_SERVER = 'http://localhost:8081'; 
+  constructor(private httpClient: HttpClient) { }
 
-
-  constructor(private httpClient: HttpClient) {}
-
-
-  private httpHeaders = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  });
-
-
-  private handleError(error: HttpErrorResponse): Observable<never> {
+  // Gestion des erreurs
+  private handleError(error: HttpErrorResponse) {	
     let errorMessage = 'Unknown Error.';
     if (error.error instanceof ErrorEvent) {
       errorMessage = `Error: ${error.error.message}`;
     } else {
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    console.error(errorMessage); 
     return throwError(errorMessage);
   }
 
-  getUserList(): Observable<any> {
-    return this.httpClient.get<Client[]>(`${this.API_SERVER}/client/all`, { headers: this.httpHeaders })
-      .pipe(catchError(this.handleError));
+  // Récupérer tous les clients
+  getAllClients(): Observable<Client[]> {
+    return this.httpClient.get<Client[]>(`${this.API_SERVER}/all`).pipe(catchError(this.handleError));
   }
 
+  // Ajouter un client
+  createClient(client: Client): Observable<Client> {
+    return this.httpClient.post<Client>(`${this.API_SERVER}/save`, client).pipe(catchError(this.handleError));
+  }
 
+  // Récupérer un client par ID
+  getClientById(id: number): Observable<Client> {
+    return this.httpClient.get<Client>(`${this.API_SERVER}/getOne/${id}`).pipe(catchError(this.handleError));
+  }
 
+  // Mettre à jour un client
+  updateClient(id: number, client: Client): Observable<Client> {
+    return this.httpClient.put<Client>(`${this.API_SERVER}/update/${id}`, client).pipe(catchError(this.handleError));
+  }
+
+  // Supprimer un client
+  deleteClient(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.API_SERVER}/delete/${id}`).pipe(catchError(this.handleError));
+  }
 }
--- src/app/Services/utilisateur.service.ts
+++ src/app/Services/utilisateur.service.ts
@@ -1,36 +1,51 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Client } from '../Interfaces/client';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UtilisateurService {
+export class ClientService {
   
-  private API_SERVER = "http://localhost:8081"
-  
+  private API_SERVER = "http://localhost:8081/client";
+
   constructor(private httpClient: HttpClient) { }
 
-  handleError(error: HttpErrorResponse) {	
+  // Gestion des erreurs
+  private handleError(error: HttpErrorResponse) {	
     let errorMessage = 'Unknown Error.';
     if (error.error instanceof ErrorEvent) {
       errorMessage = `Error: ${error.error.message}`;
-    }
-    else {
+    } else {
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-      
     return throwError(errorMessage);
-    }
+  }
 
-    getAllUsers():Observable<any> {    
-      let httpHeaders = new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Accept':  'application/json',
-      });
-  
-      return this.httpClient.get(this.API_SERVER+"/utilisateur/all", {headers: httpHeaders}).pipe(catchError(this.handleError));
-    }
+  // Récupérer tous les clients
+  getAllClients(): Observable<Client[]> {
+    return this.httpClient.get<Client[]>(`${this.API_SERVER}/all`).pipe(catchError(this.handleError));
+  }
 
+  // Ajouter un client
+  createClient(client: Client): Observable<Client> {
+    return this.httpClient.post<Client>(`${this.API_SERVER}/save`, client).pipe(catchError(this.handleError));
+  }
 
+  // Récupérer un client par ID
+  getClientById(id: number): Observable<Client> {
+    return this.httpClient.get<Client>(`${this.API_SERVER}/getOne/${id}`).pipe(catchError(this.handleError));
+  }
+
+  // Mettre à jour un client
+  updateClient(id: number, client: Client): Observable<Client> {
+    return this.httpClient.put<Client>(`${this.API_SERVER}/update/${id}`, client).pipe(catchError(this.handleError));
+  }
+
+  // Supprimer un client
+  deleteClient(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.API_SERVER}/delete/${id}`).pipe(catchError(this.handleError));
+  }
 }
